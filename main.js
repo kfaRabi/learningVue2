b2ea4961efa2c7e1ed9ff5576ea4000e362a3483
rabi <fozleazimrabi2@gmail.com>
2017-03-22 01:04:16 +0600

    parent child communication example #1

--- main.js
+++ main.js
@@ -1,79 +1,16 @@
-Vue.component('tab', {
-
-	props: {
-		name: { required: true,},
-		selected: {default: false,},
-	},
-
-	template: `
-		<div v-show="this.isActive"><slot></slot></div>
-	`,
-	
-	data(){
-		return {isActive: false};
-	},
-	//we are creating this bcz, otherwise we had to change the property of the components.
-	//Vue does not allow that. Hence, the isActive data field.
-
-	computed: {
-		getLink(){
-			return '#' + this.name.toLowerCase().replace(/ /g, '-');
-		}
-	},
-
-});
-
-Vue.component('tabs',{
+Vue.component('cupon',{
 	
 	//props: [],
 
 	template: `
-		<div>
-			<div class="tabs">
-			 	<ul>
-			    	<li v-for="tab in tabs" :class="{'is-active': tab.isActive}">
-			    		<a :href="tab.getLink" @click="selectTab(tab)">{{ tab.name }}</a>
-			    	</li>
-				</ul>
-			</div>
-			<div class="tab-details">
-				<slot></slot>
-			</div>
-		</div>
+		<input type="text" @blur = "fireEvent"/>
 	`,
 
-	data(){
-		return{
-			tabs: [],
-		};
-	},
-
-	mounted(){ // learn the diffrences btn mounted and created
-		this.tabs = this.$children;
-		// if(this.tabs.length){
-		// 	this.tabs[0].isActive = true;
-		// }
-		let flag = true;
-		for (var i = 0; i <= this.tabs.length - 1; i++) {
-			//--i changed to i++
-			if(this.tabs[i].selected){
-				this.tabs[i].isActive = true;
-				flag = false;
-				break;
-			}
-		}
-		if(this.tabs.length && flag){
-			this.tabs[0].isActive = true;
-		}
-	},
-
 	methods: {
-		selectTab(selectedTab){
-			this.tabs.forEach(tab => {
-				tab.isActive = (tab.name == selectedTab.name);
-			});
-		}	
-	}
+		fireEvent(){
+			this.$emit("applied");
+		},
+	},
 
 })
 
@@ -83,5 +20,15 @@ Vue.component('tabs',{
 
 var app = new Vue({
 	el: "#root",
+	
+	data: {
+		isApplied: false,
+	},
+
+	methods: {
+		cuponApplied(){
+			this.isApplied = true;
+		},
+	}
 
 });
