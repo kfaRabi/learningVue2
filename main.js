98e751f1a3b2adb6904cc881bde7f898c1502911
rabi <fozleazimrabi2@gmail.com>
2017-03-21 19:08:14 +0600

    Component inside another Component

--- main.js
+++ main.js
@@ -2,6 +2,32 @@ Vue.component('task', {
 	template: `<li><slot></slot></li>`,
 });
 
+Vue.component('task-list', {
+	template: `
+		<div>
+			<task v-for="task in tasks">{{ task.desc }}</task>
+		</div>
+	`,
+
+	data(){
+		return {
+			tasks: [
+				{desc: "Go to the store", done: true},
+				{desc: "Go to the university", done: true},
+				{desc: "Eat dinner", done: false},
+				{desc: "Watch tutorial", done: false},
+				{desc: "Learn Vue", done: false},
+				{desc: "Develop Social Network", done: false},
+			]
+		};
+	}
+
+});
+
+
+
+
+
 var app = new Vue({
 	el: "#root",
 });
